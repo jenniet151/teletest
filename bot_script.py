02b42495f9023bef42e736feebd05825f094cd12
jenniet151 <125991536+jenniet151@users.noreply.github.com>
2023-07-08 16:18:54 -0700

    Update and rename telesend.py to bot_script.py

---
+++ bot_script.py
@@ -0,0 +1,25 @@
+from telegram.ext import Updater, MessageHandler, Filters
+
+# Function to handle incoming messages
+def handle_message(update, context):
+    message = update.message.text
+
+    # Check if the message format matches the required pattern
+    if message.startswith('⚜️ᴄᴀʀᴅ -»') and '|' in message:
+        # Extract the relevant portion of the message
+        text = message.split('-» ')[1]
+
+        # Forward the modified message to the target group
+        context.bot.send_message(chat_id='TARGET_GROUP_ID', text='/chk ' + text)
+
+# Create an instance of the Updater and pass your bot token
+updater = Updater(token='YOUR_BOT_TOKEN', use_context=True)
+
+# Get the dispatcher to register handlers
+dispatcher = updater.dispatcher
+
+# Register the message handler
+dispatcher.add_handler(MessageHandler(Filters.text & (Filters.chat('CHANNEL_A_ID') | Filters.group('GROUP_A_ID')), handle_message))
+
+# Start the bot
+updater.start_polling()
